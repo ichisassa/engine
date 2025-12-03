ad2b443ef36b1b3bc0a784a4d811efc89e4074da
ichisassa <ichisassa@gmail.com>
2025-12-03 20:04:15 +0900

    no message

--- src/main/java/com/mingshiu/engine/controller/DisplayController.java
+++ src/main/java/com/mingshiu/engine/controller/DisplayController.java
@@ -26,22 +26,22 @@ public class DisplayController {
 
     @GetMapping("/status/base/input")
     public String baseStatusInput(Model model) {
-        return render("基礎入力", "contents/status-base-input", "status-base-input", model);
+        return render("基礎ステータス入力画面", "contents/status-base-input", "status-base-input", model);
     }
 
     @GetMapping("/status/weapon/input")
     public String baseStatusWeapon(Model model) {
-        return render("武器入力", "contents/status-weapon-input", "status-weapon-input", model);
+        return render("武器ステータス入力画面", "contents/status-weapon-input", "status-weapon-input", model);
     }
 
     @GetMapping("/status/bonus/input")
     public String baseStatusBonus(Model model) {
-        return render("ボーナス入力", "contents/status-bonus-input", "status-bonus-input", model);
+        return render("ステータスボーナス入力画面", "contents/status-bonus-input", "status-bonus-input", model);
     }
 
     @GetMapping("/status/echo/input")
     public String baseStatusEcho(Model model) {
-        return render("音骸入力", "contents/status-echo-input", "status-echo-input", model);
+        return render("音骸ステータス入力画面", "contents/status-echo-input", "status-echo-input", model);
     }
 
 }
