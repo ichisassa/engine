9cd5310d6e1cb7a48340d161bd05c2ff0ae5f2bf
ichisassa <ichisassa@gmail.com>
2025-12-07 04:31:46 +0900

    no message

--- src/main/resources/sql/initialize.sql
+++ src/main/resources/sql/initialize.sql
@@ -18,3 +18,17 @@ CREATE DATABASE mingshiu
 -- ============================================
 GRANT ALL PRIVILEGES ON DATABASE mingshiu TO mingshiu_user;
 
+-- ============================================
+-- 
+-- ============================================
+CREATE TABLE temp_file (
+    user_id      VARCHAR(50)  NOT NULL,      -- ユーザーID
+    session_id   VARCHAR(50)  NOT NULL,      -- セッションID
+    unique_id    VARCHAR(50)  NOT NULL,      -- ユニークID（UUID）
+    image_type   SMALLINT     NOT NULL,      -- 画像種別（1:メイン、2:顔）
+    file_name    VARCHAR(50)  NOT NULL,      -- 元ファイル名
+    content_type VARCHAR(50)  NOT NULL,      -- MIMEタイプ
+    file_base64  TEXT         NOT NULL,      -- Base64データ本体
+    created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,    -- 登録日時
+    CONSTRAINT pk_temp_file PRIMARY KEY (user_id, session_id, unique_id, image_type)
+);
