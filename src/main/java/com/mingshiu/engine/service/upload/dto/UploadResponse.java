101b077ce645b60011d1039e62adb739f40fce58
ichisassa <ichisassa@gmail.com>
2025-12-11 00:44:31 +0900

    no message

--- src/main/java/com/mingshiu/engine/service/upload/dto/UploadResponse.java
+++ src/main/java/com/mingshiu/engine/service/upload/dto/UploadResponse.java
@@ -1,11 +1,17 @@
 package com.mingshiu.engine.service.upload.dto;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class UploadResponse {
 
   public boolean isError;
   public Map<String, String> messages;
+
+  public UploadResponse() {
+    this.isError = false;
+    this.messages = new LinkedHashMap<>();
+  }
 
   public void success() {
     this.isError = false;
